f95a492e99ad86f9df185bb396705d3aca46656d
mahbrk08 <mahbrk08@gmail.com>
2018-04-04 10:05:46 +0300

    no message

---
+++ playground/playground/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace playground
+{
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[10];
+            
+
+            int am = 0;
+            string selection = Console.ReadLine();
+            switch (selection)
+            {
+                case "generate-array":
+                    foreach(int x in myArray)
+            {
+                Random random = new Random();
+                int rnd = random.Next(myArray.Length);
+                Console.WriteLine(myArray[rnd]);
+            }
+                    break;
+
+                case "get-array-statistics":
+                    foreach(int x in myArray)
+                    {
+                        am = myArray.Sum() / myArray.Length;
+                    }
+                    Console.WriteLine("Максимальное число: "+myArray.Max());
+                    Console.WriteLine("Минимальное число: "+myArray.Min());
+                    Console.WriteLine("Среднее арифметическое равно: "+am);
+                    break;
+                   
+                default:
+                    Console.WriteLine("Ошибка");
+                    break;
+            }
+        }
+    }
+}
