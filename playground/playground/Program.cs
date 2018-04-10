7d30114c47710395255ab8f93114ff5f48840454
mahbrk08 <mahbrk08@gmail.com>
2018-04-10 10:12:46 +0300

    no message

--- playground/playground/Program.cs
+++ playground/playground/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace playground
 {
@@ -12,32 +13,36 @@ namespace playground
 
         static void Main(string[] args)
         {
+
             int[] myArray = new int [10];
-            
             Random random = new Random();
             int am = 0;
             string selection = Console.ReadLine();
             switch (selection)
             {
                 case "generate-array":
-                    foreach(int x in myArray)
-            {
-                
-                int rnd = random.Next(myArray.Length);
-                Console.WriteLine(myArray[rnd]);
-            }
+                   
+
+                       for (int index = 0; index < myArray.Length; index++)
+                        {
+                            myArray[index] = random.Next();
+                            Console.WriteLine(myArray[index]);
+                            File.WriteAllText("C:\\Users\\i7\\source\\repos\\playground\\arrays.csv" + myArray[index]); //В этой строчке выдаёт ошибку, код - CS1501 "Ни одна из перезагрузок метода WriteAllText не принимает 1 аргументов"
+                        }
+                    
                     break;
 
                 case "get-array-statistics":
                     foreach(int x in myArray)
                     {
-                        int rnd = random.Next(myArray.Length);
+                        int rnd = random.Next();
                         am = myArray.Sum() / myArray.Length;
+                        Console.WriteLine("Максимальное число: "+myArray.Max());
+                        Console.WriteLine("Минимальное число: "+myArray.Min());
+                        Console.WriteLine("Среднее арифметическое равно: "+am);
                     }
 
-                   Console.WriteLine("Максимальное число: "+myArray.Max());
-                    Console.WriteLine("Минимальное число: "+myArray.Min());
-                    Console.WriteLine("Среднее арифметическое равно: "+am);
+                   
                     break;
                    
                 default:
