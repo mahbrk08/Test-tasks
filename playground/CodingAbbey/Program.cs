c64d43288e086e665b7a9e0e38289319e9d8a153
Youkai-no-nii-chan <andreychuk@mindbox.ru>
2018-04-21 19:31:55 +0300

    Добавил еще один проект

---
+++ playground/CodingAbbey/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CodingAbbey
+{
+    class Program
+    {
+        static void Main()
+        {
+            var arguments = Console.ReadLine().Split(' ');
+            var a = int.Parse(arguments[0]);
+            var b = int.Parse(arguments[1]);
+            Console.WriteLine(a + b);
+            Console.ReadLine();
+        }
+    }
+}
