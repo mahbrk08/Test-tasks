c42e8baa835deb375b48ad796c41b40093f1f32f
Youkai-no-nii-chan <andreychuk@mindbox.ru>
2018-05-14 23:01:05 +0300

    A plus B

---
+++ CodeAbbey/APlusB/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace APlusB
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                var array = Console.ReadLine().Split(' ');
+                
+                if (array.Length != 2)
+                {
+                    Console.WriteLine("Введите 2 значения.");
+                }
+                else if (!int.TryParse(array[0], out var a) ||
+                        !int.TryParse(array[1], out var b))
+                {
+                    Console.WriteLine("Введите числа.");
+                }
+                else
+                {
+                    var sum = a + b;
+                    Console.WriteLine(sum);
+                    Console.ReadKey();
+                    break;
+                }
+            }
+
+        }
+    }
+}
