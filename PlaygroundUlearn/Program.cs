36f57c980b12adf965cb7ea14c9fb69eb59ab1f6
mahbrk08 <mahbrk08@gmail.com>
2018-06-27 09:28:02 +0300

    Забыл как решать проблему со сплитом

---
+++ PlaygroundUlearn/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PlaygroundUlearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate();
+        }
+
+        public static double Calculate()
+        {
+            double sum = Double.Parse(Console.ReadLine().Split(' '));
+            double interestRate = Double.Parse(Console.ReadLine().Split(' '));
+            double duration = Double.Parse(Console.ReadLine());
+            double capitalazation = sum / 100 * interestRate * duration / 12;
+            Console.WriteLine(sum + capitalazation);
+            return sum + capitalazation;
+        }
+
+    }
+}
